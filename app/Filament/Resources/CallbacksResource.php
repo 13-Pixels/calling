a44b42c736a661e322025119a87e562c369804b2
danish-13digital <danish@13.pixel>
2024-04-22 15:18:28 +0500

    modify migrations and columns

--- app/Filament/Resources/CallbacksResource.php
+++ app/Filament/Resources/CallbacksResource.php
@@ -31,12 +31,11 @@ class CallbacksResource extends Resource
     public static function form(Form $form): Form
     {
         $customers = Customer::pluck('name', 'id');
-        if($form->model == 'App\Models\Callback'){
+        if ($form->model == 'App\Models\Callback') {
             $customer = null;
             $job_status = null;
-        }else{
+        } else {
             $customer = Customer::findOrFail($form->model->customer_id);
-            $job_status = $form->model->job_status;
         }
         return $form
             ->schema([
@@ -69,13 +68,13 @@ class CallbacksResource extends Resource
                             ->content(isset($customer->phone) ? $customer->phone : null),
                     ]),
                 ])
-                ->columnSpan(4)
-                ->hidden(fn (string $operation): bool => $operation === 'create'),
+                    ->columnSpan(4)
+                    ->hidden(fn(string $operation): bool => $operation === 'create'),
                 Split::make([
                     Section::make([
                         Placeholder::make('name')
-                            ->label('Booking Date & Time')    
-                            ->content(isset($customer->enquiry_date) ? $customer->enquiry_date : null),               
+                            ->label('Booking Date & Time')
+                            ->content(isset($customer->enquiry_date) ? $customer->enquiry_date : null),
                     ]),
                     Section::make([
                         Placeholder::make('name')
@@ -90,8 +89,8 @@ class CallbacksResource extends Resource
                             ->label('Discounted Prices')->content('123'),
                     ]),
                 ])
-                ->columnSpanFull()
-                ->hidden(fn (string $operation): bool => $operation === 'create'),
+                    ->columnSpanFull()
+                    ->hidden(fn(string $operation): bool => $operation === 'create'),
 
                 Section::make('')
                     ->schema([
@@ -101,55 +100,55 @@ class CallbacksResource extends Resource
                         DatePicker::make('booking_date')->label('Booking Date')->required(),
                     ])
                     ->columns(2)
-                    ->hidden(fn (string $operation): bool => $operation === 'edit'),
+                    ->hidden(fn(string $operation): bool => $operation === 'edit'),
                 Section::make('')
                     ->schema([
                         Split::make([
                             Placeholder::make('Email')
-                            ->label('Job Status')
-                            ->content(new HtmlString("<strong><span style='color: " . ($job_status === CallBackEnum::BOOKING ? 'green' : ($job_status === CallBackEnum::QUOTE ? 'orange' : 'inherit')) . ";'>$job_status</span></strong>")),
-                    ])->hidden(fn (string $operation): bool => $operation === 'create'),
+                                ->label('Job Status')
+                                ->content(new HtmlString("<strong><span style='color: " . ($job_status === CallBackEnum::BOOKING ? 'green' : ($job_status === CallBackEnum::QUOTE ? 'orange' : 'inherit')) . ";'>$job_status</span></strong>")),
+                        ])->hidden(fn(string $operation): bool => $operation === 'create'),
 
                         Select::make('job_status')->label('Job Status')
-                        ->options([
-                            'Booking' => 'Booking',
-                            'Quote' => 'Quote',
-                        ])->required()->hidden(fn (string $operation): bool => $operation === 'edit'),
+                            ->options([
+                                'Booking' => 'Booking',
+                                'Quote' => 'Quote',
+                            ])->required()->hidden(fn(string $operation): bool => $operation === 'edit'),
                         Select::make('callback_status')
-                        ->label('Callback Status')
-                        ->options([
-                            'Booked' => 'Booked',
-                            'Pending' => 'Pending',
-                            'New' => 'New',
-                            'Lost' => 'Lost',
-                        ])->required(),
+                            ->label('Callback Status')
+                            ->options([
+                                'Booked' => 'Booked',
+                                'Pending' => 'Pending',
+                                'New' => 'New',
+                                'Lost' => 'Lost',
+                            ])->required(),
                         DatePicker::make('callback_date')->label('Callback Date')->required(),
                         TextInput::make('location')->label('Location')->required(),
                     ])->columns(2),
-                ]);
+            ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
-        ->columns([
-            TextColumn::make('quote')->searchable(),
-            TextColumn::make('enquiry_date')->searchable(),
-            TextColumn::make('booking_date')->searchable(),
-            TextColumn::make('callback_date')->searchable(),
-            TextColumn::make('job_status')->searchable(),
-            TextColumn::make('job_status')
+            ->columns([
+                TextColumn::make('quote')->searchable(),
+                TextColumn::make('enquiry_date')->searchable(),
+                TextColumn::make('booking_date')->searchable(),
+                TextColumn::make('callback_date')->searchable(),
+                TextColumn::make('job_status')->searchable(),
+                TextColumn::make('job_status')
                     ->color(function (string $state) {
                         return match ($state) {
                             CallBackEnum::BOOKING => 'success',
                             CallBackEnum::QUOTE => 'warning',
                         };
                     })
-                    ->formatStateUsing(fn (string $state): string => __("{$state}"))
+                    ->formatStateUsing(fn(string $state): string => __("{$state}"))
                     ->weight('bold')
                     ->searchable(),
-            TextColumn::make('location')->searchable(),
-            TextColumn::make('callback_status')
+                TextColumn::make('location')->searchable(),
+                TextColumn::make('callback_status')
                     ->color(function (string $state) {
                         return match ($state) {
                             CallBackEnum::BOOKED => 'success',
@@ -158,7 +157,7 @@ class CallbacksResource extends Resource
                             CallBackEnum::LOST => 'danger',
                         };
                     })
-                    ->formatStateUsing(fn (string $state): string => __("{$state}"))
+                    ->formatStateUsing(fn(string $state): string => __("{$state}"))
                     ->weight('bold')
                     ->searchable(),
             ])
@@ -170,7 +169,7 @@ class CallbacksResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
--- database/migrations/2024_03_14_071825_create_callbacks_table.php
+++ database/migrations/2024_03_14_071825_create_callbacks_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,16 +12,17 @@ return new class extends Migration
     {
         Schema::create('callbacks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('customer_id')->constrained()->casecadeOnDelete();
             $table->string('quote');
             $table->date('enquiry_date');
             $table->date('booking_date');
-            $table->date('callback_date');
-            $table->enum('job_status', ['Booking', 'Quote']);
-            $table->string('location');
-            $table->enum('callback_status', ['Booked', 'Pending', 'New', 'Lost']);
-            $table->unsignedBigInteger('customer_id');
+            $table->date('callback_date')->nullable();
+            $table->string('job_status');
+            $table->string('callback_status')->nullable();
+            $table->string('pick_up');
+            $table->string('drop_off');
+            $table->string('via')->nullable();
             $table->timestamps();
-            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
         });
     }
 
