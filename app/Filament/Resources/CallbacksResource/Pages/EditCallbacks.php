4bffe3fc27c4c76cce1d61cad3d7a8afa6e256be
danish-13digital <danish@13.pixel>
2024-05-07 13:51:27 +0500

    log completed

--- app/Filament/Resources/CallbacksResource/Pages/EditCallbacks.php
+++ app/Filament/Resources/CallbacksResource/Pages/EditCallbacks.php
@@ -21,16 +21,16 @@ class EditCallbacks extends EditRecord
     }
 
     protected function handleRecordUpdate(Model $record, array $data): Model
-{
-    if($data['callback_status'] == 'lost' || $data['callback_status'] == 'booked') {
-        $data['close_date'] = date('Y-m-d');
-    };
-    // dd($data['close_date']);
-    $record->update($data);
-    $this->refreshFormData([
-            'close_date',
-        ]);
- 
-    return $record;
-}
+    {
+        if($data['callback_status'] == 'lost' || $data['callback_status'] == 'booked') {
+            $data['close_date'] = date('Y-m-d');
+        };
+        // dd($data['close_date']);
+        $record->update($data);
+        $this->refreshFormData([
+                'close_date',
+            ]);
+    
+        return $record;
+    }
 }
