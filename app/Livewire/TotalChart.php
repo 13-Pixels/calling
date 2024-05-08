e9fbc513fd9fb7ac1a63aee03fbc6b2c0bf59997
danish-13digital <danish@13.pixel>
2024-05-08 17:14:08 +0500

    total chart set

--- app/Livewire/TotalChart.php
+++ app/Livewire/TotalChart.php
@@ -3,10 +3,6 @@
 namespace App\Livewire;
 
 use App\Models\Callback;
-use Flowframe\Trend\Trend;
-use Illuminate\Support\Carbon;
-use Flowframe\Trend\TrendValue;
-use Filament\Forms\Components\DatePicker;
 use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
 
 class TotalChart extends ApexChartWidget
@@ -23,7 +19,7 @@ class TotalChart extends ApexChartWidget
      *
      * @var string|null
      */
-    protected static ?string $heading = 'TotalChart';
+    protected static ?string $heading = 'Total Chart';
 
     /**
      * Chart options (series, labels, types, size, animations...)
@@ -33,88 +29,22 @@ class TotalChart extends ApexChartWidget
      */
     protected function getOptions(): array
     {
-         $quotes = Trend::model(Callback::class)
-        ->between(
-            start: Carbon::parse($this->filterFormData['quotes_date_start']),
-            end: Carbon::parse($this->filterFormData['quotes_date_end']),
-        )
-        ->perDay()
-        ->count();
-        $booking = Trend::query(Callback::where('callback_status','booked'))
-        ->between(
-            start: Carbon::parse($this->filterFormData['booking_date_start']),
-                end: Carbon::parse($this->filterFormData['booking_date_end']),
-        )
-        ->perDay()
-        ->count();
-            $lost = Trend::query(Callback::where('callback_status','lost'))
-        ->between(
-            start: Carbon::parse($this->filterFormData['lost_date_start']),
-                end: Carbon::parse($this->filterFormData['lost_date_end']),
-        )
-        ->perDay()
-        ->count();
+        $callback = new Callback;
+        $quotes = $callback->count();
+        $booking = $callback->where('callback_status', 'booked')->count();
+        $lost = $callback->where('callback_status', 'lost')->count();
         return [
             'chart' => [
-                'type' => 'line',
+                'type' => 'pie',
                 'height' => 300,
             ],
-            'series' => [
-                [
-                    'name' => 'Quotes',
-                    'data'=>  $quotes->map(fn (TrendValue $value) => $value->aggregate),
-                    'type' => 'column',
-                ],
-                [
-                    'name' => 'Booking',
-                      'data'=> $booking->map(fn (TrendValue $value) => $value->aggregate),
-                    'type' => 'line',
-                ],
-                   [
-                    'name' => 'Lost',
-                      'data'=> $lost->map(fn (TrendValue $value) => $value->aggregate),
-                    'type' => 'column',
-                ],
-            ],
-            'stroke' => [
-                'width' => [0, 4],
-            ],
-            'xaxis' => [
-                'categories' =>  $lost->map(fn (TrendValue $value) => $value->aggregate),
-                'labels' => [
-                    'style' => [
-                        'fontFamily' => 'inherit',
-                    ],
-                ],
-            ],
-            'yaxis' => [
-                'labels' => [
-                    'style' => [
-                        'fontFamily' => 'inherit',
-                    ],
-                ],
-            ],
+            'series' => [$quotes, $booking, $lost],
+            'labels' => ['Quotes', 'Booking', 'Lost'],
             'legend' => [
                 'labels' => [
                     'fontFamily' => 'inherit',
                 ],
             ],
-        ];
-    }
-
-     protected function getFormSchema(): array
-    {
-        return [
-            DatePicker::make('quotes_date_start')
-            ->default(now()->subDays(30)->endOfDay()),
-            DatePicker::make('quotes_date_end')->default(now()),
-              DatePicker::make('booking_date_start')
-            ->default(now()->subDays(30)->endOfDay()),
-            DatePicker::make('booking_date_end')->default(now()),
-              DatePicker::make('lost_date_start')
-            ->default(now()->subDays(30)->endOfDay()),
-            DatePicker::make('lost_date_end')->default(now()),
-            
         ];
     }
 }
