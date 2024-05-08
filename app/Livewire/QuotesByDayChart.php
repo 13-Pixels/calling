84eb98a84516672835b81b62e9674213907e376d
danish-13digital <danish@13.pixel>
2024-05-08 14:39:07 +0500

    add table in callback

--- app/Livewire/QuotesByDayChart.php
+++ app/Livewire/QuotesByDayChart.php
@@ -4,39 +4,111 @@ namespace App\Livewire;
 
 use App\Models\Callback;
 use Flowframe\Trend\Trend;
+use Illuminate\Support\Carbon;
 use Flowframe\Trend\TrendValue;
-use Filament\Widgets\ChartWidget;
+use Filament\Forms\Components\DatePicker;
+use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
 
-class QuotesByDayChart extends ChartWidget
+class QuotesByDayChart extends ApexChartWidget
 {
+    /**
+     * Chart Id
+     *
+     * @var string
+     */
+    protected static ?string $chartId = 'quotesByDayChart';
+
+    /**
+     * Widget Title
+     *
+     * @var string|null
+     */
     protected static ?string $heading = 'Quotes By Day';
 
-    protected static ?string $pollingInterval = null;
-
-    protected function getData(): array
+    /**
+     * Chart options (series, labels, types, size, animations...)
+     * https://apexcharts.com/docs/options
+     *
+     * @return array
+     */
+    // protected function getOptions(): array
+    // {
+    //     $data = Trend::model(Callback::class) 
+    //         ->between(
+    //            start: Carbon::parse($this->filterFormData['date_start']), 
+    //             end: Carbon::parse($this->filterFormData['date_end']), 
+    //         )
+    //         ->perDay()
+    //         ->count(); 
+    //     return [
+    //         'chart' => [
+    //             'type' => 'line',
+    //             'height' => 300,
+    //         ],
+    //         'series' => [
+    //             [
+    //                 'name' => 'QuotesByDayChart',
+    //                 'data' => $data->map(fn (TrendValue $value) => $value->aggregate), 
+    //             ],
+    //         ],
+    //         'xaxis' => [
+    //             'labels' => [
+    //                 'style' => [
+    //                     'fontFamily' => 'inherit',
+    //                 ],
+    //             ],
+    //         ],
+    //         'yaxis' => [
+    //             'labels' => [
+    //                 'style' => [
+    //                     'fontFamily' => 'inherit',
+    //                 ],
+    //             ],
+    //         ],
+    //         'colors' => ['#f59e0b'],
+           
+    //     ];
+    // }
+      protected function getOptions(): array
     {
-        
-        $quotes = Trend::model(Callback::class)
-        ->between(
-            start: now()->startOfYear(),
-            end: now(),
-        )
-        ->perDay()
-        ->sum('total');
-
         return [
-            'datasets' => [
+            'chart' => [
+                'type' => 'line',
+                'height' => 300,
+            ],
+            'series' => [
                 [
-                    'label' => 'Quotes ',
-                    'data' => $quotes->map(fn (TrendValue $value) => $value->aggregate),
+                    'name' => 'BlogPostsChart',
+                    'data' => [7, 4, 6, 10, 14, 7, 5, 9, 10, 15, 13, 18],
                 ],
             ],
-            'labels' => $quotes->map(fn (TrendValue $value) => $value->date),
+            'xaxis' => [
+                'categories' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                        'fontWeight' => 600,
+                    ],
+                ],
+            ],
+            'yaxis' => [
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'colors' => ['#f59e0b'],
+
         ];
     }
-
-    protected function getType(): string
+       protected function getFormSchema(): array
     {
-        return 'line';
+        return [
+            DatePicker::make('date_start')
+                ->default(now()->startOfYear()),
+            DatePicker::make('date_end')
+                ->default(now()),
+        ];
     }
 }
